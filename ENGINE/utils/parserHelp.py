6a4e5d37370bc1fd40e6fb5a1b3c3ecb5c58b853
Kevin Kusiak <kkusiak516@gmail.com>
2022-02-07 08:55:19 -0500

    update

---
+++ ENGINE/utils/parserHelp.py
@@ -0,0 +1,18 @@
+from optparse import OptionParser
+
+class OptionParsingError(RuntimeError):
+    def __init__(self, msg):
+        self.msg = msg
+
+class OptionParsingExit(Exception):
+    def __init__(self, status, msg):
+        self.msg = msg
+        self.status = status
+
+
+class ModifiedOptionParser(OptionParser):
+    def error(self, msg):
+        raise OptionParsingError(msg)
+
+    def exit(self, status=0, msg=None):
+        raise OptionParsingExit(status, msg)
